652af826e7fb2fbbdf5d4a7b8fb6c5d060c53d37
Raul Holanda Lopes <raulzc00@gmail.com>
2023-12-20 19:00:03 -0300

    fix: ajustando perfil de usuário e card de pet

--- pages/perfilUsuario.js
+++ pages/perfilUsuario.js
@@ -1,106 +1,109 @@
-import Styles from "../styles/index.module.css"
-import SideBar from "../src/components/sideBar/sideBar"
-import FotoDePerfil from "../src/components/fotoDePerfil/fotoDePerfil"
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import Styles from "../styles/perfilUsuario.module.css";
+import SideBar from "../src/components/sideBar/sideBar";
+import FotoDePerfil from "../src/components/fotoDePerfil/fotoDePerfil";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 
 export default function perfilUsuario() {
+  const router = useRouter();
 
-    const router = useRouter();
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [erro, setErro] = useState("");
+  const [userData, setUserData] = useState({});
 
-    const [token, setToken] = useState(null);
-    const [userId, setUserId] = useState(null);
-    const [erro, setErro] = useState('');
-    const [userData, setUserData] = useState({});
-
-    useEffect(() => {
-
-        if (typeof window !== "undefined") {
-            const storedToken = localStorage.getItem('token');
-            if (storedToken) {
-                setToken(storedToken);
-                try {
-                    const decoded = jwtDecode(storedToken);
-                    setUserId(decoded.sub);
-                } catch (error) {
-                    console.error('Erro ao decodificar o token:', error);
-                }
-            } else {
-                console.log('Nenhum token encontrado.');
-            }
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedToken = localStorage.getItem("token");
+      if (storedToken) {
+        setToken(storedToken);
+        try {
+          const decoded = jwtDecode(storedToken);
+          setUserId(decoded.sub);
+        } catch (error) {
+          console.error("Erro ao decodificar o token:", error);
         }
-
-        async function buscarPerfil() {
-            if (token && userId) {
-                try {
-                    const response = await fetch(`http://localhost:2306/user/${userId}`, {
-                        method: 'GET',
-                        headers: {
-                            'Authorization': `Bearer ${token}`
-                        }
-                    });
-
-                    if (response.status === 401) {
-                        throw new Error('Usuário não logado!');
-                    } else if (response.status === 403) {
-                        throw new Error('Acesso negado!');
-                    } else if (!response.ok) {
-                        throw new Error('Erro ao buscar informações do usuário');
-                    }
-
-                    const dadosPerfil = await response.json();
-                    setUserData(dadosPerfil);
-                } catch (error) {
-                    setErro(error.message);
-                }
-            }
-        }
-
-        buscarPerfil();
-
-    }, [token, userId]);
-
-
-
-
-    const handleEdit = async (e) => {
-        router.push('/infoUser');
+      } else {
+        console.log("Nenhum token encontrado.");
+      }
     }
 
-   
-    
-    return (
-        <div className={Styles.container}>
-            <SideBar />
-            <div className={Styles.perfil}>
-                <div className={Styles.botoesNav}>
-                    <button className={Styles.link} onClick={handleEdit}>
-                        informações Pessoais
-                    </button>
-                    
+    async function buscarPerfil() {
+      if (token && userId) {
+        try {
+          const response = await fetch(`http://localhost:2306/user/${userId}`, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
 
-                </div>
-                <div className={Styles.vemDeLadinho}>
-                    <FotoDePerfil h="110" w="140" />
-                    <div className={Styles.infosPrincipais}>
-                        <h3 className={Styles.nome}> <strong className={Styles.nickname}>@{userData.username}</strong> - {userData.nome}</h3>
-                        <h4> {userData.cidade} - {userData.estado}</h4>
-                    </div>
-                </div>
+          if (response.status === 401) {
+            throw new Error("Usuário não logado!");
+          } else if (response.status === 403) {
+            throw new Error("Acesso negado!");
+          } else if (!response.ok) {
+            throw new Error("Erro ao buscar informações do usuário");
+          }
 
-                <div className={Styles.infoUsuario}>
+          const dadosPerfil = await response.json();
+          setUserData(dadosPerfil);
+        } catch (error) {
+          setErro(error.message);
+        }
+      }
+    }
 
-                    <div className={Styles.campo}>
-                        <strong>Biografia</strong><br></br>
-                        <span className={Styles.quebra}>{userData.biografia}</span>
-                    </div>
+    buscarPerfil();
+  }, [token, userId]);
 
-                </div>
-            
+  const handleEdit = async (e) => {
+    router.push("/infoUser");
+  };
+
+  return (
+    <div className={Styles.html}>
+      <div className={Styles.container}>
+        <SideBar />
+        <div className={Styles.perfil}>
+          <div className={Styles.botoesNav}>
+            <button className={Styles.link} onClick={handleEdit}>
+              informações Pessoais
+            </button>
+          </div>
+          <div className={Styles.vemDeLadinho}>
+            <FotoDePerfil h="150" w="200" />
+            <div className={Styles.infosPrincipais}>
+              <h3 className={Styles.nome}>
+                <strong className={Styles.nome}></strong>
+                {userData.nome}
+              </h3>
+              <h3 className={Styles.nickname}>
+                <strong className={Styles.nome}></strong>@{userData.username}
+              </h3>
+              <h4 className={Styles.email}>
+                {userData.email}
+              </h4>
+              <h4 className={Styles.email}>
+                {userData.cidade}
+              </h4>
+              <h4 className={Styles.email}>
+                {userData.estado}
+              </h4>
             </div>
+          </div>
 
-
+          <div className={Styles.infoUsuario}>
+            <div className={Styles.campo}>
+              <strong>Biografia</strong>
+              <br></br>
+              <span className={Styles.quebra}>{userData.biografia}</span>
+            </div>
+          </div>
+          <div className={Styles.barra}></div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -1,90 +1,114 @@
-import Styles from "./post.module.css"
+import Styles from "./post.module.css";
 import Image from "next/image";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
-
 export default function Post({ petId, nome, username, descricao }) {
+  const router = useRouter();
+  const idDoPet = petId;
+  const [pet, setPet] = useState({});
+  const [token, setToken] = useState("");
+  const [erro, setErro] = useState("");
 
-    const router = useRouter();
-    const idDoPet = petId;
-    const [pet, setPet] = useState({});
-    const [token, setToken] = useState('');
-    const [erro, setErro] = useState('');
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("token");
 
-    useEffect(() => {
-        
-
-        if (typeof window !== "undefined") {
-            const token = localStorage.getItem('token');
-            
-            if (token) {
-                try {
-                    console.log('a porra do token do post:', token)
-                    setToken(token)
-                    
-
-                } catch (error) {
-                    console.error('Erro ao decodificar o token:', error);
-                }
-            } else {
-                console.log('Nenhum token encontrado.');
-            }
+      if (token) {
+        try {
+          console.log("a porra do token do post:", token);
+          setToken(token);
+        } catch (error) {
+          console.error("Erro ao decodificar o token:", error);
         }
-        fetchPets()
-        console.log('idzin',idDoPet)
-        console.log('tokenzin',token)
-        async function fetchPets() {
-       
-            try {
-                const response = await fetch(`http://localhost:2306/animal/${idDoPet}`, {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                });
+      } else {
+        console.log("Nenhum token encontrado.");
+      }
+    }
+    fetchPets();
+    console.log("idzin", idDoPet);
+    console.log("tokenzin", token);
+    async function fetchPets() {
+      try {
+        const response = await fetch(
+          `http://localhost:2306/animal/${idDoPet}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
-                if (response.status === 401) {
-                    throw new Error('Usuário não logado!');
-                } else if (response.status === 403) {
-                    throw new Error('Acesso negado!');
-                } else if (!response.ok) {
-                    throw new Error('Erro ao buscar pets');
-                }
-
-                const petiscos = await response.json();
-                setPet(petiscos)
-                console.log('dados do animal: ',petiscos)
-            } catch (error) {
-                setErro(error.message);
-            }
+        if (response.status === 401) {
+          throw new Error("Usuário não logado!");
+        } else if (response.status === 403) {
+          throw new Error("Acesso negado!");
+        } else if (!response.ok) {
+          throw new Error("Erro ao buscar pets");
         }
-    }, [idDoPet, token]);
 
-    
-    
-    
-    return (
-        <div className={Styles.post}>
-            <div className={Styles.postHeader}>
-                <img
-                    src="/peraul.jpg"
-                    alt={`Foto do dono`}
-                />
-                <div>
-                    <div className={Styles.name}>{nome}</div>
-                    <div className={Styles.timestamp}>@{username}</div>
-                </div>
-                <div className={Styles.raiPaLa}><button className={Styles.petButton} onClick={() => router.push(`/perfilPet/${idDoPet}`)}>@{pet.nome}</button></div>
-            </div>
-            <div className={Styles.postContent}>
-                {descricao}
-            </div>
-            <div className={Styles.postFooter}>
-                <span>Like</span>
-                <span>Comment</span>
-                <span>Share</span>
-            </div>
+        const petiscos = await response.json();
+        setPet(petiscos);
+        console.log("dados do animal: ", petiscos);
+      } catch (error) {
+        setErro(error.message);
+      }
+    }
+  }, [idDoPet, token]);
+
+  return (
+    <div className={Styles.post}>
+      <div className={Styles.postHeader}>
+        <img src="/peraul.jpg" alt={`Foto do ${nome}`} />
+        <div>
+          <div className={Styles.name}>{nome}</div>
+          <div className={Styles.timestamp}>@{username}</div>
         </div>
-    );
+        <div className={Styles.raiPaLa}>
+          <button
+            className={Styles.petButton}
+            onClick={() => router.push(`/perfilPet/${idDoPet}`)}
+          >
+            @{pet.nome}
+          </button>
+        </div>
+      </div>
+      <div className={Styles.postContent}>{descricao}</div>
+      <div className={Styles.postFooter}>
+        <div className={Styles.heartContainer} title="Like">
+          <input
+            type="checkbox"
+            className={Styles.checkbox}
+            id="Give-It-An-Id"
+          />
+          <div className={Styles.svgContainer}>
+            {/* Assegure-se de que os SVGs têm o atributo xmlns definido corretamente */}
+            <svg
+              viewBox="0 0 24 24"
+              className={Styles.svgOutline}
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {/* Seu caminho SVG */}
+            </svg>
+            <svg
+              viewBox="0 0 24 24"
+              className={Styles.svgFilled}
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {/* Seu caminho SVG */}
+            </svg>
+            <svg
+              className={Styles.svgCelebrate}
+              width="100"
+              height="100"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {/* Seus polígonos SVG */}
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
